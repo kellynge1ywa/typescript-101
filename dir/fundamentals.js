e48bb3e36b28f8eedb1b19c261d377055c380258
kellynge1ywa <120782457+kellynge1ywa@users.noreply.github.com>
2023-10-04 13:42:37 +0300

    typescript 101

---
+++ dir/fundamentals.js
@@ -0,0 +1,7 @@
+"use strict";
+let matunda = ["Chungwa", "Apple", "Mango", "Avocado", "Grapes", "Passion"];
+let tafuta = matunda.find((tunda) => tunda == "Grapes");
+console.log(tafuta);
+let nummer = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let hesabu = nummer.reduce((acc, num) => acc * num);
+console.log(hesabu);
